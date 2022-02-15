07d3271c57987fefd53d23ecaa3f1cf4ebc97bd4
Yevhenii <freshjs.fte.@gmail.com>
2022-02-15 17:07:07 +0200

    Add big O examples

---
+++ assets/js/bigO.js
@@ -0,0 +1,26 @@
+  // O(n)
+for(let i = 1; i < n; i++) {
+  if (true) {}
+}
+
+// O(n^2)
+for(let i = 1; i < n; i++) {
+  for (let j = 1; j < n; i++) {
+    if (true) {}
+  }
+}
+
+// O(n^2)
+array.map((v) => {
+  if (array2.includes(v))
+    return v;
+})
+
+// O(n);
+let res = 1;
+if (array2.includes(v))
+    res = v;
+
+array.map((v) => {
+  res
+})
