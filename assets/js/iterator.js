5429ddc9691297be2383b9944d5c4fbd2ef17717
Yevhenii <freshjs.fte.@gmail.com>
2022-02-18 17:06:05 +0200

    Add generator example

--- assets/js/iterator.js
+++ assets/js/iterator.js
@@ -1,28 +1,16 @@
-const iter = function () {
-  let index = Math.trunc(this.length / 2);
-  const end = this.length;
-  
-  return {
-    next: () => {
-      if (index < end) {
-        return {
-          value: this[index++] ,
-          done: false,
-        };
-      }
+const gen = function* () {
+  const arr = this;
 
-      return {
-        value: undefined,
-        done: true,
-      }
-    }
+  for (let i = 0; i < arr.length; i++) {
+    yield arr[i];
+    break;
   }
 }
 
 const arr = [1, 2, 5, 1, 32, 6];
 
-arr[Symbol.iterator] = iter;
+arr[Symbol.iterator] = gen;
 
-for(const value of arr) {
-  console.log(value)
+for (const value of arr) {
+  console.log(value);
 }
