d8cf1d867fbc5a6d50bc0ef0826bbe84726ede2e
Yevhenii <freshjs.fte.@gmail.com>
2022-02-21 14:42:36 +0200

    Add my array methods solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,91 @@
-try {
-  throw new RangeError('random error')
-} catch (err) {
-
-  if(err instanceof TypeError) {
-    console.error(err)
-  }
-
-  if(err instanceof RangeError) {
-    console.error(err)
-    alert('попробуйте еше раз')
-  }
-} finally {
-  console.log('final');
-}
-
 // prototype obj
 const arrayPrototype = {
   // Class.prototype.includes
-  includes: function (arg) {
-    for(let i = 0; i < this.length; i++) {
-      if (this.array[i] === arg) {
+  includes: function (searchElement, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this.array[i] === searchElement) {
         return true;
       }
     }
+
     return false;
   },
 
-
-  indexOf: function () {
-    for(let i = 0; i < this.length; i++) {
-      if (this.array[i] === arg) {
+  indexOf: function (searchElement, fromIndex = 0) {
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this.array[i] === searchElement) {
         return i;
       }
     }
+
     return -1;
-  }
-}
+  },
 
+  slice: function (begin = 0, end = 0) {
+    if (begin < 0) {
+      const newBegin = this.length + begin;
+      begin = newBegin < 0 ? 0 : newBegin;
+    }
 
+    if (end < 0) {
+      const newEnd = this.length + end;
+      end = newEnd < 0 ? 0 : newEnd;
+    }
 
+    const newArr = new MyArray();
+
+    for (let i = begin; i < end; i++) {
+      newArr.array[i - begin] = this.array[begin];
+      newArr.length++;
+    }
+
+    return newArr;
+  },
+  push: function (item) {
+    if (item === undefined) {
+      return this.length;
+    }
+
+    this.array[this.length] = item;
+    this.length++;
+
+    return this.length;
+  },
+
+  concat: function (item) {
+    if (item === undefined) {
+      return Object.assign(new MyArray(), this);
+    }
+
+    const newArr = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      newArr.array[i] = this.array[i];
+      newArr.length++;
+    }
+    
+    if (item.length) {
+      for (let i = 0; i < item.length; i++) {
+
+        if (MyArray.isMyArray(item)) {
+          newArr.array[this.length + i] = item.array[i];
+        } else if (Array.isArray(item)) {
+          newArr.array[this.length + i] = item[i];
+        }
+
+        newArr.length++;
+      }
+    } else {
+      newArr[newArr.length] = item;
+      newArr.length++;
+    }
+
+    return newArr;
+  },
+};
 
 // constructor func
 function MyArray() {
-  this.array = {}
+  this.array = {};
   this.length = 0;
 
   for (let i = 0; i < arguments.length; i++) {
@@ -53,10 +96,7 @@ function MyArray() {
 }
 
 // bind prototype obj (Class.prototype)
-MyArray.prototype = Object.assign(
-  MyArray.prototype, 
-  arrayPrototype
-);
+MyArray.prototype = Object.assign(MyArray.prototype, arrayPrototype);
 
 // static method (Class.method)
 MyArray.isMyArray = (arg) => {
@@ -64,17 +104,14 @@ MyArray.isMyArray = (arg) => {
     return true;
   }
   return false;
-}
-
-
-
+};
 
 // create instance (new)
-const myarray = new MyArray()
+const myarray = new MyArray();
 
-myarray.array[0] = 4
-myarray.length = 1
+myarray.array[0] = 4;
+myarray.length = 1;
 
-const result = myarray.includes(4)
+const result = myarray.includes(4);
 
 console.log(result);
